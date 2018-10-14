7aa52f477b5fc96bfe538884f0fb6a084c585453
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-10-14 16:30:26 -0500

    asdas

---
+++ gameJamSlightlyUsed/Assets/MainMenuButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour {
+
+
+	void Update () {
+        MappedInput.InputDevices.ForEach(x =>
+        {
+            if (x.GetButtonDown(MappedButton.Ready))
+            {
+                LevelManager.Instance.LoadNextLevel();
+            }
+        });
+        
+    }
+}
